a7b83a88b2b526f24411f307af2badbaad4e5fa5
Eduardo Huszcz <124888040+DudsHuszcz@users.noreply.github.com>
2026-04-27 09:37:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def hello():
     return "Olá, Professor! A aplicação está rodando via Docker!"
 
+# Funções extras apenas para os testes unitários
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
